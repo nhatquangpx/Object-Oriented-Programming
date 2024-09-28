aeebc154c6b8c4cde1e94d2f6c35e6cf64a5c9d5
nhatquangpx <doannhatquang0@gmail.com>
2024-09-29 00:01:07 +0700

    Xoa file .class

--- Lab_01/SolveEquation.java
+++ Lab_01/SolveEquation.java
@@ -1,7 +1,30 @@
 // Example 5: SolveEquation.java
+import javax.swing.JOptionPane;
 public class SolveEquation {
     public static void main(String[] args) {
-        
+        String OptionalEquation;
+        double a = 0;
+        OptionalEquation = JOptionPane.showInputDialog(null,"Doan Nhat Quang - Choose your equation: 1 = First-degree equation (1 variable) ; 2 = Second-degree equation (1 variable) ; 3 = First-degree equations (2 variables)","Option",JOptionPane.INFORMATION_MESSAGE);
+        double Opt = Double.parseDouble(OptionalEquation);
+        while (condition) {
+      
+        switch (Opt) {
+            case 1:
+                
+                break;
+
+            case 2:
+
+                break;
+            
+            case 3:
+
+                break;
+            
+            default:
+                break;
+        }
+
     }
     
 }
